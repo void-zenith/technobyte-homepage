cbb67103a83f60a9614b4aaaa7f53abf3edd10b7
SamikshyaBasnet <68534684+SamikshyaBasnet@users.noreply.github.com>
2021-12-13 14:24:57 +0545

    Responsive Home

--- src/Components/Mainbody/Mainbody.jsx
+++ src/Components/Mainbody/Mainbody.jsx
@@ -3,38 +3,47 @@ import Code from "../../assets/image/code";
 import Partilce from "./particle";
 import { ArrowRight } from 'react-bootstrap-icons';
 import DownArrow from "../DownArrow";
-import Divider from "@material-ui/core/Divider";
 import { Link } from "react-scroll";
+
+import { Row, Col, Container } from "react-bootstrap";
 const Mainbody = () => {
   return (
     <div id="home" className="mainbody-container">
-      <div className="mainbody-container__left">
-        <div className="left-container__content">
-          <div className="technobyte">Technobyte Nepal</div>
-          <div className="catchphrase">
-            <h1>
-              We build softwares for{" "}
-              <span className="catchphrase-for">startups</span> &{" "}
-              <span className="catchphrase-for">Businesses.</span>
-            </h1>
-          </div>
-          <div className="messaage">
-            Leveraging our design, web, and mobile development team, we help
-            business build end to end digital products.
-          </div>
-          <div className="letsworktogether">
-            <Link to="how_we_work" smooth={true} duration={1000}>Let's work together</Link>
-
-            <ArrowRight style={{ marginLeft: "6px", fontSize: "20" }} />
-
-          </div>
-        </div>
-
+      <div className="content">
+        <Row className="justify-content-md-center">
+          <Col xs="12" md="auto">
+            <div className="mainbody-container__left">
+              <div className="left-container__content">
+                <div className="d-flex">
+                  <div className="technobyte">Technobyte Nepal</div>
+                </div>
+                <div className="catchphrase">
+                  <h1 className="text-responsive">
+                    We build softwares for{" "}
+                    <br />
+                    <span className="catchphrase-for">startups</span> &{" "}
+                    <span className="catchphrase-for">Businesses.</span>
+                  </h1>
+                </div>
+                <div className="messaage">
+                  <p className="text-responsive text-center">
+                    Leveraging our design, web, and mobile development team,<br /> we help
+                    businesses build end to end digital products.
+                  </p>
+                </div>
+                <div className="d-flex">
+                  <div className="letsworktogether">
+                    <Link to="how_we_work" smooth={true} duration={1000}>Let's work together</Link>
+                    <ArrowRight style={{ marginLeft: "6px", fontSize: "20" }} />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <DownArrow></DownArrow>
+          </Col>
+        </Row>
       </div>
-      <div className="mainbody-container__right">
-        <Code></Code>
-      </div>
-      <DownArrow></DownArrow>
+
     </div>
   );
 };
